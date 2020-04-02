dbc7423cc2d897da84beefd821a3a5f13fe9c575
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-04-02 17:12:20 -0500

    add and test shop feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -98,9 +98,63 @@ var endGame = function() {
         }
 }
 
+var shop = function() {
+    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store?  Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+
+    switch (shopOptionPrompt) {
+
+        case "REFILL":
+        case "refill":
+            if (playerMoney >= 7){
+                window.alert("Refilling player's health by 20 for 7 dollars");
+
+                // increase health and decrease money
+                playerHealth = playerHealth + 20;
+                playerMoney = playerMoney - 7;
+            }
+
+            else {
+                window.alert("You don't have enough money");
+            }
+
+            break;
+
+        case "UPGRADE":
+        case "upgrade":
+            if (playerMoney >= 7){
+
+                window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+                // increase attack and decrease money
+                playerAttack = playerAttack + 6;
+                playerMoney = playerMoney -7;
+            }
+
+            else {
+                window.alert("You don't have enough money");
+            }
+
+            break;
+
+        case "LEAVE":
+        case "leave":
+            window.alert("Leaving the store");
+
+            // do nothing, so function will end
+            break;
+
+        default:
+            window.alert("You did not pick a valid option.  Try again.")
+
+            // call shop() again to force player to pick a valid option.
+            shop();
+            break;
+    }
+}
+
 // Function to start a new game.
 var startGame = function() {
-    debugger;
+//    debugger;
 
     // Reset player Stats
     playerHealth = 100;
@@ -114,6 +168,14 @@ var startGame = function() {
             var pickedEnemyName = enemyNames[i];
             enemyHealth = 50;
             fight(pickedEnemyName);
+
+            if (playerHealth > 0 && i < enemyNames.length - 1) {
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+                if (storeConfirm) {
+                    shop();
+                }
+            }
         }
 
         else {
